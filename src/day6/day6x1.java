ecaff1f87fcd98c8eb3686ac4774912ec7f3ca83
stephan <00735276@edu.campus02.at>
2022-01-16 23:21:06 +0100

    Approach with Hashmaps and usage of BigInteger instead of Integer allowed the calculation of the lampfish population. Dax6x2 solved!

--- src/day6/day6x1.java
+++ src/day6/day6x1.java
@@ -1,29 +1,47 @@
 package day6;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
 
 public class day6x1 {
     public static void main(String[] args) {
         //List<Integer> lanternfish = new ArrayList<>(Arrays.asList(3, 4, 3, 1, 2));
         List<Integer> lanternfish = utils.inputReaders.readSingleLineIntegerInput("src/day6/day6x1.txt");
         System.out.println(lanternfish);
+        Map<Integer, BigInteger> fishpond = new HashMap<>();
+        for (Integer fish:lanternfish) {
+           if (fishpond.containsKey(fish)){
+               BigInteger fishcount = fishpond.get(fish);
+               fishcount = fishcount.add(BigInteger.valueOf(1));
+               fishpond.put(fish, fishcount);
+           }
+           else {
+               fishpond.put(fish, BigInteger.valueOf(1));
+           }
+        }
         int days = 1;
         while (days <= 256){
-            List<Integer> nextDaysFish = new ArrayList<>();
-            for (Integer fish:lanternfish) {
-               if (0<fish){
-                   nextDaysFish.add(fish-1);
-               }
-               else if (fish == 0){
-                   nextDaysFish.add(8);
-                   nextDaysFish.add(6);
-               }
+            HashMap<Integer, BigInteger> newpond = new HashMap<>();
+            for (Integer key:fishpond.keySet()) {
+                if (key == 0){
+                    newpond.put(6, fishpond.get(0));
+                    newpond.put(8, fishpond.get(0));
+                }
+
+                else if (0<key){
+                    BigInteger newStock = fishpond.get(key);
+                    switch (key){
+                        case 7:
+                            if (newpond.containsKey(key-1)){
+                                newStock = newStock.add(newpond.get(key-1));
+                            }
+                            break;
+                    }
+                    newpond.put(key-1, newStock);
+                }
             }
-           // System.out.println("Day "+days+ ": "+ nextDaysFish);
-            System.out.println("Day "+ days + ": " + nextDaysFish.size() + " fish");
-            lanternfish = nextDaysFish;
+            fishpond = newpond;
+            System.out.println("Day "+days+": "+fishpond.values().stream().reduce(BigInteger.ZERO, BigInteger::add));
             days++;
         }
     }
